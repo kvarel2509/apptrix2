1334a3e929b976a190f462b4bfc7a919395dcf9d
Alexey <kvarel@mail.ru>
2022-02-11 20:20:49 +0300

    finish

--- apptrix2/settings.py
+++ apptrix2/settings.py
@@ -139,7 +139,7 @@ EMAIL_USE_SSL = False
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer'
+        # 'rest_framework.renderers.BrowsableAPIRenderer'
     ),
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
